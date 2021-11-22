35f8be07802150921caaef408118338f71e650b8
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-11-22 23:10:30 +0000

    Add BDD test

---
+++ SpecFlowTests/Features/ChooseVaccine.feature
@@ -0,0 +1,7 @@
+﻿Feature: Choose Best Vaccine
+
+A short summary of the feature
+
+Scenario: Choose Vaccine For Young Female
+	Given I am 22-year old female
+	Then my vaccine is "Johnson And Johnson"
---
+++ SpecFlowTests/StepDefinitions/StepDefinitions.cs
@@ -0,0 +1,29 @@
+﻿using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyNamespace
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        CovidVaccineEligibility.VaccineEligibility vaccineEligibility;
+        public StepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"I am (.*)-year old female")]
+        public void GivenIAm_YearOldFemale(int age)
+        {
+            vaccineEligibility = new CovidVaccineEligibility.VaccineEligibility();
+            vaccineEligibility.Gender = CovidVaccineEligibility.Gender.Female;
+            vaccineEligibility.Age = age;
+        }
+
+        [Then(@"my vaccine is ""(.*)""")]
+        public void ThenMyVaccineIs(string vaccineName)
+        {
+            Assert.IsTrue(vaccineEligibility.Vaccine.Contains(vaccineName));
+        }
+    }
+}

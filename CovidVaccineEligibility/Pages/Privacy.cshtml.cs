1722e9d966810549083aabcb0690d25f94c045d4
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-11-23 01:13:55 +0000

    Remove unnecessary code

--- CovidVaccineEligibility/Pages/Privacy.cshtml.cs
+++ CovidVaccineEligibility/Pages/Privacy.cshtml.cs
@@ -1,19 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace CovidVaccineEligibility.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            this.logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
     }
 }
